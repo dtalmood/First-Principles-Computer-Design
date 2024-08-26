53a88e4e8e30c829cb8b3e98edf5e1cc28a14f47
dtalmood <47163162+dtalmood@users.noreply.github.com>
2024-08-26 15:13:34 -0700

    Assembly Program
    Currently it can
    1. determine if C or A instruction
    2. If A find if its a decimal

--- Phase6/Assembler.py
+++ Phase6/Assembler.py
@@ -1,17 +1,38 @@
-print("Hello")
+# Open the .asm file for reading.
+file1 = open("TestFiles/Add.asm", "r")
 
-# Open the .asm file for reading and writing.
-file1 = open("TestFiles/Add.asm", "w+")
-
-# this is the file we are going to be writing to.
+# This is the file we are going to be writing to.
 file2 = open("Output/Add.asm", "w")
 
-
-#loop file 1 line by line 
+# Loop through file1 line by line
 line = file1.readline()
 
-while(line):
-    print(line.strip())
+while line:
+    mainLine = line.strip() # Remove leading/trailing whitespace 
+
+    if mainLine == "" or mainLine.startswith("//"): # Check Comment or Line 
+        line = file1.readline()
+        continue  # Skip Rest of Code 
+
+    # Determine if A-instruction or C-instruction
+    
+    if mainLine.startswith('@'): # A-Instruction
+       
+         if(mainLine[1:2].isnumeric()): # Decimal
+            print("Found Decimal")
+
+        
+
+        # Lablel
+
+        # Symbol 
+    
+    else: # C-instruction
+        print("C")
+
+    # Read the next line
     line = file1.readline()
-    
-    
+
+# Close the files after the operation
+file1.close()
+file2.close()
