10d9805f0fad8f344aac3e65ba4a3871c75d735f
dtalmood <47163162+dtalmood@users.noreply.github.com>
2024-08-26 13:49:06 -0700

    Test Casees Added

--- Phase6/Assembler.py
+++ Phase6/Assembler.py
@@ -1,7 +1,17 @@
 print("Hello")
 
-# Open the .asm file for reading
-file1 = open("prog.asm", "w+")
+# Open the .asm file for reading and writing.
+file1 = open("TestFiles/Add.asm", "w+")
 
-#file we are going to be writing to
-file = open("prog.txt", "w")
+# this is the file we are going to be writing to.
+file2 = open("Output/Add.asm", "w")
+
+
+#loop file 1 line by line 
+line = file1.readline()
+
+while(line):
+    print(line.strip())
+    line = file1.readline()
+    
+    
--- Phase6/TestFiles/Max.asm
+++ Phase6/TestFiles/Max.asm
@@ -1,23 +1,26 @@
-// Computes RAM[2] =
-// max(RAM[0],RAM[1])
-@R0
-D=M
-@R1
-D=D-M
-@OUTPUT_RAM0
-D;JGT
-// Output RAM[1]
-@R1
-D=M
-@R2
-M=D
-@END
-0;JMP
-(OUTPUT_RAM0)
-@R0
-D=M
-@R2
-M=D
-(END)
-@END
-0;JMP
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/06/max/Max.asm
+
+// Computes M[2] = max(M[0], M[1])  where M stands for RAM
+
+   @0
+   D=M              // D = first number
+   @1
+   D=D-M            // D = first number - second number
+   @OUTPUT_FIRST
+   D;JGT            // if D>0 (first is greater) goto output_first
+   @1
+   D=M              // D = second number
+   @OUTPUT_D
+   0;JMP            // goto output_d
+(OUTPUT_FIRST)
+   @0             
+   D=M              // D = first number
+(OUTPUT_D)
+   @2
+   M=D              // M[2] = D (greatest number)
+(INFINITE_LOOP)
+   @INFINITE_LOOP
+   0;JMP            // infinite loop
