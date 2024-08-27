28a8731bf24a8fec9e1ee5a07679d6db30208892
dtalmood <47163162+dtalmood@users.noreply.github.com>
2024-08-26 20:22:45 -0700

    Updated to Assembler.py
    1. it can not write To output file in with file extension .hack
    
    2. can identify, and write to output file

--- Phase6/Assembler.py
+++ Phase6/Assembler.py
@@ -1,38 +1,102 @@
-# Open the .asm file for reading.
-file1 = open("TestFiles/Add.asm", "r")
+# Predefined symbols equate to memory locations
+preDefinedSymbols = {
+    'SP': 0,      'LCL': 1,    'ARG': 2,   'THIS': 3,   'THAT': 4,
+    'R0': 0,      'R1': 1,    'R2': 2,    'R3': 3,    'R4': 4,
+    'R5': 5,      'R6': 6,    'R7': 7,    'R8': 8,    'R9': 9,
+    'R10': 10,    'R11': 11,  'R12': 12,  'R13': 13,  'R14': 14,
+    'R15': 15,    'SCREEN': 16384, 'KBD': 24576
+}
+userDefinedSymbols= {}
 
-# This is the file we are going to be writing to.
-file2 = open("Output/Add.asm", "w")
+def convertDecimalToBinary(value):
+    print("Entered Function")
+    count = 0
+    current = 0 
+    answer = 0
+    while(value != 0):
+        if(value % 2 == 1):
+            current = 1
+        else:
+            current = 0
+        
+        answer += current * (10 ** count)
+        count += 1
+        value //= 2
+    rest = 15-count+1
+    final_answer = '0' * rest + str(answer)
+    print("Final Answer:", final_answer)
+    return final_answer
 
-# Loop through file1 line by line
-line = file1.readline()
+def AInstruction(mainLine):
+    print("A-Instruction")
 
-while line:
-    mainLine = line.strip() # Remove leading/trailing whitespace 
-
-    if mainLine == "" or mainLine.startswith("//"): # Check Comment or Line 
-        line = file1.readline()
-        continue  # Skip Rest of Code 
-
-    # Determine if A-instruction or C-instruction
+    # Decimal
+    if mainLine[1:2].isnumeric():  
+        print("Found Decimal")
+        value = int(mainLine[1:])
+        result = convertDecimalToBinary(value)
+        return result
     
-    if mainLine.startswith('@'): # A-Instruction
-       
-         if(mainLine[1:2].isnumeric()): # Decimal
-            print("Found Decimal")
+    # Check if Predefined Symbol 
 
         
 
-        # Lablel
+def LInstruction(mainLine, pc):
+    print("L-Instruction")
+    userDefinedSymbols[mainLine] = pc 
+    result = convertDecimalToBinary(pc) 
+    return result
 
-        # Symbol 
+
+
+
+def CInstruction(mainLine):
+    print("C-Instruction")
+
+
+def main():
+    # Open the .asm file for reading.
+    file1 = open("TestFiles/test.asm", "r")
     
-    else: # C-instruction
-        print("C")
-
-    # Read the next line
+    # This is the file we are going to be writing to.
+    file2 = open("Output/test.hack", "w+")
+    
     line = file1.readline()
 
-# Close the files after the operation
-file1.close()
-file2.close()
+    pc = 0
+
+    # Loop through file1 line by line
+    while line:
+        mainLine = line.strip()  # Remove leading/trailing whitespace 
+        
+        if mainLine == "" or mainLine.startswith("//"):  # Comment or Empty Line 
+            line = file1.readline()
+            continue  # Skip Rest of code 
+        # Determine if A, C or L instruction 
+        
+        if mainLine.startswith('@'):  # A-inst
+            answer = AInstruction(mainLine)
+            file2.write(f"{answer}\n")
+
+
+        elif mainLine.startswith('(') and mainLine[-1] == ")":  #L-Inst        
+            answer = LInstruction(mainLine, pc)
+            file2.write(f"{answer}\n")
+        
+        else:  # C-Inst
+            CInstruction(mainLine) 
+
+        # Read the next line
+        line = file1.readline()
+        #increment our program counter
+        pc += 1
+
+    # Close the files after the operation
+    file1.close()
+    file2.close()
+
+if __name__ == "__main__":
+    main()
+
+
+
